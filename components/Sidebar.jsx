61fc8a80611d446dd349c8eb6e8e469bf2050bc3
Fagbenja Enoch <fagbenjaenoch73@gmail.com>
2023-04-28 20:57:02 +0100

    update: finished the sidebar

--- components/Sidebar.jsx
+++ components/Sidebar.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import {AiOutlineDown} from 'react-icons/ai'
-import {TbRectangle, TbCircle, TbTriangle} from 'react-icons/tb'
+import {TbRectangle, TbCircle, TbTriangle, TbSettings, TbPaperclip, TbMoneybag} from 'react-icons/tb'
 
 import SearchBar from './SearchBar'
 
@@ -13,7 +13,7 @@ const Sidebar = () => {
       <SearchBar/>
       <span className='text-gray-500 mb-4'>Menu</span>
 
-      <div className='flex gap-5 items-center hover:bg-gray-900 p-2 rounded-lg mb-2'>
+      <div className='flex gap-5 items-center text-gray-500 hover:text-white cursor-pointer hover:bg-gray-900 p-2 rounded-lg mb-2'>
         <TbRectangle size={25}/>
         <div className='w-full flex items-center justify-between'>
           <div>Overview</div>
@@ -21,7 +21,7 @@ const Sidebar = () => {
         </div>
       </div>
 
-      <div className='flex gap-5 items-center hover:bg-gray-900 p-2 rounded-lg mb-2'>
+      <div className='flex gap-5 items-center text-gray-500 hover:text-white cursor-pointer hover:bg-gray-900 p-2 rounded-lg mb-2'>
         <TbRectangle size={25}/>
         <div className='w-full flex items-center justify-between'>
           <div>Transactions</div>
@@ -29,7 +29,7 @@ const Sidebar = () => {
         </div>
       </div>
 
-      <div className='flex gap-5 items-center hover:bg-gray-900 p-2 rounded-lg mb-2'>
+      <div className='flex gap-5 items-center text-gray-500 hover:text-white cursor-pointer hover:bg-gray-900 p-2 rounded-lg mb-2'>
         <TbCircle size={25}/>
         <div className='w-full flex items-center justify-between'>
           <div>Schedule Alerts</div>
@@ -37,7 +37,7 @@ const Sidebar = () => {
         </div>
       </div>
 
-      <div className='flex gap-5 items-center hover:bg-gray-900 p-2 rounded-lg mb-2'>
+      <div className='flex gap-5 items-center text-gray-500 hover:text-white cursor-pointer hover:bg-gray-900 p-2 rounded-lg mb-2'>
         <TbTriangle size={25}/>
         <div className='w-full flex items-center justify-between'>
           <div>Cashstack</div>
@@ -45,34 +45,44 @@ const Sidebar = () => {
         </div>
       </div>
 
-      <div className='flex items-center justify-between hover:bg-gray-900 p-2 rounded-lg mb-2'>
-        <div>Overview </div>
-        <AiOutlineDown className='cursor-pointer'/>
+      <div className='border-t border-gray-500 py-2'>
+        <div className='flex gap-5 items-center text-gray-500 hover:text-white cursor-pointer hover:bg-gray-900 p-2 rounded-lg'>
+          <TbMoneybag size={25}/>
+          <div className='w-full flex items-center justify-between'>
+            <div>Finances</div>
+            <AiOutlineDown className='cursor-pointer'/>
+          </div>
+        </div>
       </div>
 
-      <div className='flex items-center justify-between hover:bg-gray-900 p-2 rounded-lg mb-2'>
-        <div>Overview </div>
-        <AiOutlineDown className='cursor-pointer'/>
+      <div className='border-t border-gray-500 py-2'>
+        <div className='flex gap-5 items-center text-gray-500 hover:text-white cursor-pointer hover:bg-gray-900 p-2 rounded-lg'>
+          <TbPaperclip size={25}/>
+          <div className='w-full flex items-center justify-between'>
+            <div>Contracts</div>
+            <AiOutlineDown className='cursor-pointer'/>
+          </div>
+        </div>
       </div>
 
-      <div className='flex items-center justify-between hover:bg-gray-900 p-2 rounded-lg mb-2'>
-        <div>Overview </div>
-        <AiOutlineDown className='cursor-pointer'/>
+      <div className='border-t border-gray-500 py-2'>
+        <div className='flex gap-5 items-center text-gray-500 hover:text-white cursor-pointer hover:bg-gray-900 p-2 rounded-lg'>
+          <TbSettings size={25}/>
+          <div className='w-full flex items-center justify-between'>
+            <div>Settings</div>
+            <AiOutlineDown className='cursor-pointer'/>
+          </div>
+        </div>
       </div>
 
-      <div className='flex items-center justify-between hover:bg-gray-900 p-2 rounded-lg mb-2'>
-        <div>Overview </div>
-        <AiOutlineDown className='cursor-pointer'/>
-      </div>
-
-      <div className='flex items-center justify-between hover:bg-gray-900 p-2 rounded-lg mb-2'>
-        <div>Overview </div>
-        <AiOutlineDown className='cursor-pointer'/>
-      </div>
-
-      <div className='flex items-center justify-between hover:bg-gray-900 p-2 rounded-lg mb-2'>
-        <div>Overview </div>
-        <AiOutlineDown className='cursor-pointer'/>
+      <div className='border-t border-gray-500 py-2'>
+        <div className='flex gap-5 items-center text-gray-500 hover:text-white cursor-pointer hover:bg-gray-900 p-2 rounded-lg'>
+          <TbCircle size={25}/>
+          <div className='w-full flex items-center justify-between'>
+            <div>Status</div>
+            <AiOutlineDown className='cursor-pointer'/>
+          </div>
+        </div>
       </div>
     </div>
   )
