be8ebbb80be18df7e748e84113751ca9a31e8f81
Fagbenja Enoch <fagbenjaenoch73@gmail.com>
2023-04-29 14:55:43 +0100

    updated the searchbar component

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -34,7 +34,10 @@ const SearchBar = () => {
       className={`flex items-center p-1 mb-4 border-b border-gray-800 text-gray-800 ${
         isHovered && !isFocused && "text-gray-400 border-gray-400"
       }
-      ${isHovered && isFocused && "text-gray-100 border-gray-200"}`}
+      ${isHovered && isFocused && "text-gray-100 border-gray-200"}
+      ${isHovered && isFocused && "placeholder:text-white"} 
+      ${isFocused && "placeholder:text-white text-gray-100 border-gray-200"}
+      `}
     >
       <button type="submit">
         <FaSearch className="mr-2" />
@@ -46,7 +49,9 @@ const SearchBar = () => {
         onChange={(e) => setSearchTerm(e.target.value)}
         className={`bg-transparent text-gray-800 outline-none w-full placeholder:text-gray-800 text-gray-100 ${
           isHovered && !isFocused && "placeholder:text-gray-500"
-        } ${isHovered && isFocused && "placeholder:text-white"}`}
+        } ${isHovered && isFocused && "placeholder:text-white"} ${
+          isFocused && "placeholder:text-white"
+        }`}
         onFocus={handleFocus}
         onBlur={handleBlur}
       />
