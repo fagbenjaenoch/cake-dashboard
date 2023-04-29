7ef772dc7855af06681d6159ef9c68dd10af6126
Fagbenja Enoch <fagbenjaenoch73@gmail.com>
2023-04-29 14:47:24 +0100

    updated the searchbar component

--- components/SearchBar.jsx
+++ components/SearchBar.jsx
@@ -1,18 +1,40 @@
 import React, { useState } from "react";
 import { FaSearch } from "react-icons/fa";
 
-function handleSubmit(e) {
-  e.preventDefault();
-  onSearch(searchTerm);
-}
-
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState("");
+  const [isHovered, setIsHovered] = useState(false);
+  const [isFocused, setIsFocused] = useState(false);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  const handleFocus = () => {
+    setIsFocused(true);
+  };
+
+  const handleBlur = () => {
+    setIsFocused(false);
+  };
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
+  };
 
   return (
     <form
       onSubmit={handleSubmit}
-      className="flex items-center p-1 mb-4 border-b border-gray-800 text-gray-800 focus-within:text-white focus-within:border-gray-200"
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+      className={`flex items-center p-1 mb-4 border-b border-gray-800 text-gray-800 ${
+        isHovered && !isFocused && "text-gray-400 border-gray-400"
+      }
+      ${isHovered && isFocused && "text-gray-100 border-gray-200"}`}
     >
       <button type="submit">
         <FaSearch className="mr-2" />
@@ -22,7 +44,11 @@ const SearchBar = () => {
         placeholder="Search"
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
-        className="bg-transparent text-gray-800 outline-none w-full placeholder:text-gray-800"
+        className={`bg-transparent text-gray-800 outline-none w-full placeholder:text-gray-800 text-gray-100 ${
+          isHovered && !isFocused && "placeholder:text-gray-500"
+        } ${isHovered && isFocused && "placeholder:text-white"}`}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
       />
     </form>
   );
