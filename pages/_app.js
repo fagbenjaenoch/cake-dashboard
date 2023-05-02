7bcd433dbe67c1bb373b7c9f36fee512882872e8
Fagbenja Enoch <fagbenjaenoch73@gmail.com>
2023-05-02 02:51:42 +0100

    update: mobile warning update

--- pages/_app.js
+++ pages/_app.js
@@ -1,40 +1,7 @@
 import "@/styles/globals.css";
 
 import Sidebar from "../components/Sidebar";
-
-import { useState, useEffect } from "react";
-
-function MobileWarning() {
-  const [isMobile, setIsMobile] = useState(false);
-
-  useEffect(() => {
-    const handleResize = () => {
-      if (window.innerWidth < 768) {
-        setIsMobile(true);
-      } else {
-        setIsMobile(false);
-      }
-    };
-    window.addEventListener("resize", handleResize);
-    handleResize();
-    return () => window.removeEventListener("resize", handleResize);
-  }, []);
-
-  if (isMobile) {
-    return (
-      <div className="backdrop-filter backdrop-blur-lg text-white p-4 fixed bottom-0 left-0 w-full z-50 text-center">
-        <div className="bg-white p-2">
-          Warning: This website is not optimized for mobile devices.
-          <button className="bg-blue-700 hover:bg-blue-600 shadow-lg text-white py-2 px-3 rounded-lg">
-            It&apos;s fine
-          </button>
-        </div>
-      </div>
-    );
-  } else {
-    return null;
-  }
-}
+import MobileWarning from "../components/MobileWarning";
 
 export default function App({ Component, pageProps }) {
   return (
